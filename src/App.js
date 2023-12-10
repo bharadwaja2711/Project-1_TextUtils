22ca19acf85ed938ccb7e3c825e1439e65a34e6f
bharadwaja2711 <110170133+bharadwaja2711@users.noreply.github.com>
2023-12-10 20:02:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+//import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+/*import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";*/
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor = 'light';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title = "TextUtils" aboutText = "AboutApp" mode = {mode} toggleMode={toggleMode}/>
+    <div className="container my-3">
+        <TextForm heading = "Enter the text to analyze"/>
+      {/*<About/>*/}
     </div>
+    </>
   );
 }
 
